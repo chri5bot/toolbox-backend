2c45d32a71004fde99c709c325027e4d08794c09
Christian Torres Avila <chris@Christians-MacBook-Pro.local>
2023-05-08 15:20:25 -0500

    fix eslint

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const port = process.env.PORT || 3000;
-const secret = process.env.EXTERNAL_API_KEY;
-const externalAPI = process.env.EXTERNAL_API;
+const port = process.env.PORT || 3000
+const secret = process.env.EXTERNAL_API_KEY
+const externalAPI = process.env.EXTERNAL_API
 
-const express = require("express");
-const axios = require("axios");
+const express = require('express')
+const axios = require('axios')
 
-const app = express();
+const app = express()
 
-app.get("/files/list", async (req, res) => {
+app.get('/files/list', async (req, res) => {
   try {
     const response = await axios.get(`${externalAPI}/files`, {
       headers: {
-        Authorization: `Bearer ${secret}`,
-      },
-    });
-    res.send(response.data);
+        Authorization: `Bearer ${secret}`
+      }
+    })
+    res.send(response.data)
   } catch (error) {
-    console.error(error);
-    res.status(error.response.status).send(error.response.data);
+    console.error(error)
+    res.status(error.response.status).send(error.response.data)
   }
-});
+})
 
-app.get("/file/:filename", async (req, res) => {
-  const filename = req.params.filename;
-  const url = `${externalAPI}/file/${filename}`;
-
-  console.log(url);
+app.get('/file/:filename', async (req, res) => {
+  const filename = req.params.filename
+  const url = `${externalAPI}/file/${filename}`
 
   try {
     const response = await axios.get(url, {
       headers: {
-        Authorization: `Bearer ${secret}`,
-      },
-    });
-    res.send(response.data);
+        Authorization: `Bearer ${secret}`
+      }
+    })
+    res.send(response.data)
   } catch (error) {
-    console.error(error);
-    res.status(error.response.status).send(error.response.data);
+    console.error(error)
+    res.status(error.response.status).send(error.response.data)
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Server listening at http://localhost:${port}`)
+})
 
-module.exports = app;
+module.exports = app
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,50 +1,48 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../app");
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
 
-chai.use(chaiHttp);
-const expect = chai.expect;
+chai.use(chaiHttp)
+const expect = chai.expect
 
-describe("GET /files/list", () => {
-  it("should return an array of files", function (done) {
+describe('GET /files/list', () => {
+  it('should return an array of files', function (done) {
     chai
       .request(app)
-      .get("/files/list")
-      .end(function (err, res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.an("object");
-        expect(res.body.files).to.be.an("array");
-        done();
-      });
-  });
-});
+      .get('/files/list')
+      .end(function (_err, res) {
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('object')
+        expect(res.body.files).to.be.an('array')
+        done()
+      })
+  })
+})
 
-describe("GET /files/:filename", () => {
-  it("should return file content on valid request", (done) => {
-    const filename = "test2.csv";
-
-    chai
-      .request(app)
-      .get(`/file/${filename}`)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.text).to.include("file,text,number,hex");
-        done();
-      });
-  });
-
-  it("should return error status and message on invalid request", (done) => {
-    const filename = "nonexistent.csv";
+describe('GET /files/:filename', () => {
+  it('should return file content on valid request', (done) => {
+    const filename = 'test2.csv'
 
     chai
       .request(app)
       .get(`/file/${filename}`)
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.text).to.include("Not Found");
-        done();
-      });
-  });
-});
+      .end((_err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.text).to.include('file,text,number,hex')
+        done()
+      })
+  })
+
+  it('should return error status and message on invalid request', (done) => {
+    const filename = 'nonexistent.csv'
+
+    chai
+      .request(app)
+      .get(`/file/${filename}`)
+      .end((_err, res) => {
+        expect(res).to.have.status(404)
+        expect(res.text).to.include('Not Found')
+        done()
+      })
+  })
+})
