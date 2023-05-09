1050ab208d0d1b58d0aabbf355945fac6c089592
Chris <chris.mood92@gmail.com>
2023-05-08 20:20:35 -0500

    add cors implementation

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const externalAPI = process.env.EXTERNAL_API
 
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.get('/files/list', async (req, res) => {
   try {
